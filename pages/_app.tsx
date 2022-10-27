b0cd59f5574b7e49364fe768b9792a58e4c8430a
Petar Cvetko Voćanec <cvetkovocanecpetar@gmail.com>
2022-10-27 19:10:07 +0200

    Removed Chakra UI since all components will be custom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,7 @@
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
 
 import '../styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<ChakraProvider>
-			<Component {...pageProps} />
-		</ChakraProvider>
-	)
+	return <Component {...pageProps} />
 }
