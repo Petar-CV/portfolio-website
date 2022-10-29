083b20b7addaa7902844dd12f5383117df106601
Petar Cvetko Voćanec <cvetkovocanecpetar@gmail.com>
2022-10-29 17:02:09 +0200

    Added footer slide in animation

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 type Props = {}
 
 export default function Footer({}: Props) {
 	return (
-		<footer className="sticky bottom-2 w-full">
+		<motion.footer
+			initial={{ opacity: 0, y: 100 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ duration: 1 }}
+			viewport={{ once: true }}
+			className="sticky bottom-2 w-full"
+		>
 			<div className="flex justify-center items-center text-center text-gray-400 text-sm md:text-base">
 				<h4>
 					Copyright &copy; {new Date().getFullYear()} | Petar Cvetko Voćanec
 				</h4>
 			</div>
-		</footer>
+		</motion.footer>
 	)
 }
