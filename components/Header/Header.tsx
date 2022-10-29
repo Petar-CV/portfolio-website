b78f02c5b284c814fb40241cc5a9523763b6c537
Petar Cvetko Voćanec <cvetkovocanecpetar@gmail.com>
2022-10-29 16:39:10 +0200

    Email icon in header now leads to the contact section

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import { SocialIcon } from 'react-social-icons'
 import { motion } from 'framer-motion'
 
@@ -30,32 +31,34 @@ export default function Header({}: Props) {
 				></SocialIcon>
 			</motion.div>
 
-			<motion.div
-				initial={{
-					x: 500,
-					opacity: 0,
-					scale: 0.5,
-				}}
-				animate={{
-					x: 0,
-					opacity: 1,
-					scale: 1,
-				}}
-				transition={{
-					duration: 1.5,
-				}}
-				className="flex flex-row items-center text-gray-300 cursor-pointer"
-			>
-				<SocialIcon
-					className="cursor-pointer"
-					network="email"
-					fgColor="gray"
-					bgColor="transparent"
-				></SocialIcon>
-				<p className="uppercase hidden md:inline-flex text-sm text-gray-400">
-					Get in touch
-				</p>
-			</motion.div>
+			<Link href="#contact">
+				<motion.div
+					initial={{
+						x: 500,
+						opacity: 0,
+						scale: 0.5,
+					}}
+					animate={{
+						x: 0,
+						opacity: 1,
+						scale: 1,
+					}}
+					transition={{
+						duration: 1.5,
+					}}
+					className="flex flex-row items-center text-gray-300 cursor-pointer"
+				>
+					<SocialIcon
+						className="cursor-pointer"
+						network="email"
+						fgColor="gray"
+						bgColor="transparent"
+					></SocialIcon>
+					<p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+						Get in touch
+					</p>
+				</motion.div>
+			</Link>
 		</header>
 	)
 }
