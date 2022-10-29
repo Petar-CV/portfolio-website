70fe9997b398c17d034bcbf3a10d8e35cbd3ea35
Petar Cvetko Voćanec <cvetkovocanecpetar@gmail.com>
2022-10-29 16:34:31 +0200

    Improved responsiveness of the Projects component

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -39,7 +39,7 @@ export default function Projects({}: Props) {
 							</motion.div>
 
 							<div className="space-y-10 px-0 md:px-10 max-w-6xl">
-								<h4 className="text-4xl font-semibold text-center ">
+								<h4 className="text-xl md:text-4xl font-semibold text-center">
 									<span className="underline decoration-red-700/50 mr-2">
 										Project {i + 1} of {projects.length}:
 									</span>
