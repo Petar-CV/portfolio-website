ebd58e315d0ed4f2b1c883dcbcbc49aba60343c8
Petar Cvetko Voćanec <cvetkovocanecpetar@gmail.com>
2022-10-30 09:50:27 +0100

    Implemented typings for the Sanity CMS entities

--- typings.d.ts
+++ typings.d.ts
@@ -15,45 +15,51 @@ interface ISanityImage {
 
 export interface IExperience extends ISanityBody {
 	_type: 'experience'
-	company: string
-	companyImage: ISanityImage
-	jobTitle: string
-	technologies: ITechnology[]
-	url: string
+	company?: string
+	companyImage?: ISanityImage
+	currentEmployer?: boolean
+	dateStarted?: string
+	dateEnded?: string
+	skillsAcquired?: string[]
+	jobTitle?: string
+	technologies?: ISkill[]
+	url?: string
 }
 
 export interface IPageInfo extends ISanityBody {
 	_type: 'pageInfo'
-	address: string
-	email: string
-	heroImage: ISanityImage
-	name: string
-	profilePicture: ISanityImage
-	role: string
+	address?: string
+	backgroundInformation?: string
+	typewriterWords?: string[]
+	email?: string
+	heroImage?: ISanityImage
+	name?: string
+	profilePicture?: ISanityImage
+	role?: string
 }
 
 export interface IProject extends ISanityBody {
 	_type: 'project'
-	image: ISanityImage
-	summary: string
-	technologies: ITechnology[]
-	title: string
+	image?: ISanityImage
+	summary?: string
+	technologies?: ISkill[]
+	title?: string
 }
 
 export interface ISkill extends ISanityBody {
 	_type: 'skill'
-	experience: ISkillExperience
-	image: ISanityImage
-	name: string
+	experience?: ISkillExperience
+	image?: ISanityImage
+	name?: string
 }
 
 export interface ISkillExperience extends ISanityBody {
 	_type: 'skillExperience'
-	skillDescription: string
+	skillDescription?: string
 }
 
 export interface ISocial extends ISanityBody {
 	_type: 'social'
-	title: string
-	url: string
+	title?: string
+	url?: string
 }
