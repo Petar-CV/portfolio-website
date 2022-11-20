12ef463275f2bbdc1b245ab05041ef46b37cdd85
Petar Cvetko Voćanec <cvetkovocanecpetar@gmail.com>
2022-11-20 17:37:04 +0100

    Hovering on skill badges now shows their name in the tooltip

--- components/Skills/Badge/SkillBadge.tsx
+++ components/Skills/Badge/SkillBadge.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
+import Tooltip from '@mui/material/Tooltip';
 
 import { ISkill } from '../../../typings';
 import { urlFor } from '../../../sanity';
@@ -11,21 +12,23 @@ type Props = {
 
 export default function SkillBadge({ skill }: Props) {
   return (
-    <div className="group relative flex cursor-pointer">
-      <motion.div
-        initial={{
-          opacity: 0,
-        }}
-        transition={{ duration: 1.2 }}
-        whileInView={{ opacity: 1 }}>
-        <Image
-          className="rounded-full bg-gray-100 border border-gray-500 h-16 w-16 md:h-24 md:w-24 xl:h-32 xl:w-32 object-cover filter group-hover:brightness-75 group-hover:grayscale transition duration-500 ease-in-out"
-          src={urlFor(skill.image).url()}
-          alt={skill.name ?? ''}
-          width={128}
-          height={128}
-        />
-      </motion.div>
-    </div>
+    <Tooltip title={skill.name}>
+      <div className="group relative flex cursor-pointer">
+        <motion.div
+          initial={{
+            opacity: 0,
+          }}
+          transition={{ duration: 1.2 }}
+          whileInView={{ opacity: 1 }}>
+          <Image
+            className="rounded-full bg-gray-100 border border-gray-500 h-16 w-16 md:h-24 md:w-24 xl:h-32 xl:w-32 object-cover filter group-hover:brightness-75 group-hover:grayscale transition duration-500 ease-in-out"
+            src={urlFor(skill.image).url()}
+            alt={skill.name ?? ''}
+            width={128}
+            height={128}
+          />
+        </motion.div>
+      </div>
+    </Tooltip>
   );
 }
